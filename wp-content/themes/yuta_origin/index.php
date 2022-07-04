d225f7c3b8162b72898ff68cce5a93b82986ef2a
Yuta Ishikawa <yutaishikawaBG>
2022-07-04 10:41:47 +0900

    初級４回

--- wp-content/themes/yuta_origin/index.php
+++ wp-content/themes/yuta_origin/index.php
@@ -50,67 +50,35 @@
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
 
-                    <?php the_post(); ?>
+                    
+                    <?php while(have_posts()):the_post(); ?>
+                        <!-- Post preview-->
+                        <div class="post-preview">
+                            <a href="post.html">
+                                <h2 class="post-title">
+                                    <?php the_title(); ?>
+                                </h2>
+                                <h3 class="post-subtitle">
+                                    <?php the_excerpt(); ?>
+                                </h3>
+                            </a>
+                            <p class="post-meta">
+                                Posted by
+                                <?php the_author(); ?>
+                                on <?php the_time("Y年n月j日l"); ?>
+                            </p>
+                        </div>
+                        <!-- Divider-->
+                        <hr class="my-4" />
+                    <?php endwhile; ?>
 
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">
-                                <?php the_title(); ?>
-                            </h2>
-                            <h3 class="post-subtitle">
-                                <?php the_content(); ?>
-                            </h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <?php the_author(); ?>
-                            on <?php the_time("Y年n月j日l"); ?>
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html"><h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2></a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 18, 2022
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Science has not yet mastered prophecy</h2>
-                            <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the next ten.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on August 24, 2022
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Failure is not an option</h2>
-                            <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on July 8, 2022
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
+
+                   
                     <!-- Pager-->
-                    <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
+                    <?php echo paginate_links(); ?>
+                    <br/>
+                    <?php previous_posts_link(); ?>
+                    <?php next_posts_link(); ?>
                 </div>
             </div>
         </div>
