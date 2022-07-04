450ae06d3732c2f8bac9e2c8fc06937fda3a8d71
Yuta Ishikawa <yutaishikawaBG>
2022-07-04 14:54:46 +0900

    itemというポストタイプの追加

--- wp-content/themes/yuta_origin/functions.php
+++ wp-content/themes/yuta_origin/functions.php
@@ -2,6 +2,19 @@
 function init_func(){
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+
+    register_post_type('item',
+        [
+            'labels' => [
+                'name' => '商品'
+            ],
+            'public' => true,
+            'has_archive' => true,
+            'hierarchical' => false,
+            'menu_position' => 5,
+            'menu_icon' => ''
+        ]
+    );
 }
 add_action('init','init_func');
 ?>
